00229e3b3f1a8900f0487114d8623ef148149415
Adae24 <dae.24@msn.com>
2026-02-01 20:58:55 +0700

    Add files via upload

---
+++ Exercise4_Dae_K.py
@@ -0,0 +1,11 @@
+Foundation_English = 85
+General_Business = 85
+Introduction_to_Computer_System = 85
+Computer_Programming = 85
+
+print("---Your Score---")
+print(Foundation_English)
+print("Foundation_English :", Foundation_English)
+print("General_Business :", General_Business)
+print("Introduction_to_Computer_System:", Introduction_to_Computer_System)
+print("Computer_Programming:", Computer_Programming)
