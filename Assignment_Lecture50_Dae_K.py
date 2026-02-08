ef36ac82535bea48248d266a78b6c1150151a45f
Adae24 <dae.24@msn.com>
2026-02-08 23:33:24 +0700

    Add files via upload

---
+++ Assignment_Lecture50_Dae_K.py
@@ -0,0 +1,5 @@
+def addNumber(x,y):                     #การรับค่าต่างๆจะใส่ในวงเล็บ เช่นกรณีนี้คือ x กับ y // parameter คือช่องรับข้อมูล
+    print(x+y)
+    print(x*y)
+    print(x/y)
+addNumber(10, 20)
